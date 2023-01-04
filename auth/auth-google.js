da692856a39acd33e28f50b8e99344155bb2f34d
Roulianosss <julien.ortscheid@gmail.com>
2023-01-04 14:33:15 +0100

    test pour deploeiment

--- auth/auth-google.js
+++ auth/auth-google.js
@@ -5,6 +5,8 @@ if (!credentials) {
   throw new Error('The $CREDS environment variable was not found!');
 }
 const keys = JSON.parse(credentials);
+const client_email = process.env.CLIENT_EMAIL
+const private_key = process.env.PRIVATE_KEY
 
 const scopes = [
   "https://www.googleapis.com/auth/drive",
@@ -12,8 +14,8 @@ const scopes = [
 ];
 
 const auth = new google.auth.JWT(
-  keys.client_email, null,
-  keys.private_key, scopes
+  client_email, null,
+  private_key, scopes
 );
 
 module.exports = { auth };

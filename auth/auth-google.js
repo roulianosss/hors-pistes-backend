19fc740c2d00645dbf8f56c08ec0df775303e2ff
Roulianosss <julien.ortscheid@gmail.com>
2023-01-04 14:48:16 +0100

    test deploiment 12

--- auth/auth-google.js
+++ auth/auth-google.js
@@ -1,10 +1,6 @@
 const { google } = require('googleapis');
 
-// const credentials = process.env['CREDS'];
-// if (!credentials) {
-//   throw new Error('The $CREDS environment variable was not found!');
-// }
-// const keys = JSON.parse(credentials);
+
 const client_email = process.env.CLIENT_EMAIL
 const private_key = process.env.PRIVATE_KEY
 
